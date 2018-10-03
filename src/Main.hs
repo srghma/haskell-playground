a7d6ba5c75622f025884878b9bb49fd918973d93
srghma <srghma@gmail.com>
2018-10-03 20:58:04 +0300

    feat: type Computation = WriterT Text (WriterT [Integer] (Writer [Double]))

--- src/Main.hs
+++ src/Main.hs
@@ -12,22 +12,29 @@ program1 = WriterT (Identity ((), ""))
 program12 :: WriterT Integer Identity ()
 program12 = WriterT (Identity ((), 1))
 
-program2 :: WriterT Text (WriterT Integer Identity) ()
-program2 = WriterT (WriterT (Identity (((), ""), 1)))
+type Computation = WriterT Text (WriterT [Integer] (Writer [Double]))
+
+program2 :: Computation ()
+program2 =
+  let content = ((((), "9999"), [9999]), [9999.0])
+  in WriterT . WriterT . WriterT . Identity $ content
 
 program4 :: WriterT Integer (WriterT Text Identity) ()
 program4 = WriterT (writer (((), 1), ""))
 
-program :: WriterT Text (WriterT [Integer] Identity) ()
-program = do
-  lift $ tell [1]
-  tell "test"
-  return ()
+program :: Computation ()
+program =
+  replicateM_ 3 $ do
+    lift . lift $ tell [1.0]
+    lift $ tell [1]
+    tell "test"
+    pass (WriterT . WriterT . WriterT . Identity $ (((((), const "gg"), "5"), [5]), [5.0]))
+    return ()
 
-interpret :: WriterT Text (WriterT [Integer] Identity) a -> ([Integer], Text)
+interpret :: Computation a -> ([Integer], [Double], Text)
 interpret c =
-    let ((_, text), list) = runWriter (runWriterT c)
-    in (list, text)
+    let (((_, text), integers), doubles) = runWriter (runWriterT (runWriterT c))
+    in (integers, doubles, text)
 
 main :: IO ()
 main = print (interpret program)
